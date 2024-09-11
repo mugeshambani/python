a830fb149b0323a2751d8f2c2528d9d16e914dfa
mugeshambani <mugeshambani1512@gmail.com>
2024-09-10 21:29:12 -0700

    Add files via upload

---
+++ fibo.h
@@ -0,0 +1,12 @@
+def recur_fibo(n)
+  if n<=1
+  return n
+else
+  return(recur_fibo(n-1)+recur_fibo(n-2))
+n term=10
+if nterm<=0
+print("please enter a integer value")
+else
+print("fibonacci sequence";)
+      for i in range(n terms):
+      print(recur_fibo(i) 
